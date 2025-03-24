ced7739279be21588d0e45156dd87bbe0ac412fc
exhertz <cactuseveryday@tuta.io>
2025-03-24 06:51:48 +0100

    statvfs info (disk) - get all, used and free bytes from disk

---
+++ disk.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/statvfs.h>
+
+#include "disk.h"
+
+typedef struct {
+  unsigned int timestamp;
+  unsigned long long used;
+} RamInfo;
+
+static struct statvfs buf;
+
+int get_statvfs() {
+  if (statvfs("/", &buf) != 0) {
+    perror("statvfs: ");
+    return 0;
+  }
+
+  return 1;
+}
+
+unsigned long get_disk_total_bytes() {
+  if (!get_statvfs())
+    return 0;
+
+  printf("totla bytes ---> %d\n", buf.f_blocks);
+
+  return buf.f_blocks * buf.f_frsize;
+}
+
+unsigned long get_disk_free_bytes() {
+  if (!get_statvfs())
+    return 0;
+
+  return buf.f_bfree * buf.f_frsize;
+}
+
+unsigned long get_disk_used_bytes()  {
+  return get_disk_total_bytes()-get_disk_free_bytes();
+}
+
+double get_disk_usage() {
+  return (double)get_disk_used_bytes()/get_disk_total_bytes();
+}
---
+++ disk.h
@@ -0,0 +1,6 @@
+#pragma once
+
+unsigned long get_disk_total_bytes();
+unsigned long get_disk_free_bytes();
+unsigned long get_disk_used_bytes();
+double get_disk_usage();
